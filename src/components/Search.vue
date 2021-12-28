75c39b94a3ddeaec7954c176d70fa4dd997b52ae
wmy <821058656@qq.com>
2021-12-28 12:01:14 +0800

    “axios-demo”

---
+++ src/components/Search.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="jumbotron">
+        <h3 class="jumbotron-heading">Search Github Users</h3>
+        <div>
+          <input type="text" placeholder="enter the name you search" v-model="keyword"/>&nbsp;
+          <button @click="searchUsers">Search</button>
+        </div>
+    </section>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name:'Search',
+    data() {
+        return {
+            keyword:''
+        }
+    },
+    methods: {
+        searchUsers(){
+            this.$bus.$emit('updateList',{
+                    isFirst:false,
+                    isLoading:true,
+                    errMsg:'',
+                    users:[]
+                })
+            axios.get(`https://api.github.com/search/users?q=${this.keyword}`).then(
+                response =>{
+                    // console.log('success',response.data.items);
+                    this.$bus.$emit('updateList',{
+                        isLoading:false,
+                        errMsg:'',
+                        users:response.data.items
+                    })
+                },
+                reject =>{
+                    this.$bus.$emit('updateList',{
+                        isLoading:false,
+                        errMsg:'error.message',
+                        users:[]
+                    })
+                }
+            )
+        }
+    },
+}
+</script>
